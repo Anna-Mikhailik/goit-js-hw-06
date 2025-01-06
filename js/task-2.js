b03ecf946c846a14165005fef51762ef2d9963b8
annplay <messageforannolay@gmail.com>
2025-01-07 00:15:42 +0200

    002

--- js/task-2.js
+++ js/task-2.js
@@ -3,17 +3,19 @@ class Storage {
   constructor(params) {
     this.#items = params;
   }
+
   get items() {
     return this.#items;
   }
+
   addItem(newItem) {
     this.#items.push(newItem);
   }
+
   removeItem(itemToRemove) {
     this.#items = this.#items.filter((item) => item !== itemToRemove);
   }
 }
-
 const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
 console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
 
